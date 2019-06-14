de4f976ce6261a3d7fc3862dbb826ecd7bfcb5f2
locatw <locatw@users.noreply.github.com>
2019-06-15 00:20:47 +0900

    add "add todo" feature

--- todo_app/app/controllers/api/todos_controller.rb
+++ todo_app/app/controllers/api/todos_controller.rb
@@ -1,5 +1,14 @@
 class Api::TodosController < ApplicationController
+  # Disable since this app doesn't have user-authentication function.
+  protect_from_forgery with: :null_session
+
+  def create
+    Todo.create!(text: params.require(:text))
+
+    render body: nil, status: :created
+  end
+
   def index
-    render json: [{id: 0, text: 'todo1'}]
+    render json: Todo.all
   end
 end
